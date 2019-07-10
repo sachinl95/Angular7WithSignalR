d54bdf074b880f95d1beef48cba53f4008811121
Sachin Pathirana <sachin@datacaptureexperts.com.au>
2019-07-10 11:29:15 +0530

    complete app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/components/common/messageservice';
+import * as signalR from '@aspnet/signalr';
+import { type } from 'os';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[MessageService]
 })
-export class AppComponent {
-  title = 'angular-client';
+export class AppComponent implements OnInit {
+
+  constructor(private messageService:MessageService){}
+  ngOnInit(): void {
+    const connection = new signalR.HubConnectionBuilder()
+    .configureLogging(signalR.LogLevel.Information)
+    .withUrl("http://localhost:28764/notify")
+    .build();
+    
+    connection.start().then(()=>{
+      console.log('Connected!');
+    }).catch((err)=>{
+      return console.error(err.toString());
+    });
+
+    connection.on("BroadcastMessage",(type:string,payload:string)=>{
+      this.messageService.add({severity:type,summary:payload,detail:'Via SignalR'})
+    })
+  }
+
 }
